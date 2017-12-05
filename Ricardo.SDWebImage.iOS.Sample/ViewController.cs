b6043ebc8e9e5c9ebaccde9221ce245a65dac2d8
Branislav Havrila <branislav.havrila@ricardo.ch>
2017-12-05 22:57:47 +0100

    Working sample project

---
+++ Ricardo.SDWebImage.iOS.Sample/ViewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Foundation;
+using Ricardo.SDWebImage.iOS;
+using UIKit;
+
+namespace Ricardo.SDWebImage.iOS.Sample
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            ImageView.Sd_setImageWithURL(new NSUrl("https://img.ricardostatic.ch/t_600/pl/764675504/0/1/14-carats-et-moins-gelbgoldring-mit-brillant-6298"));
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ Ricardo.SDWebImage.iOS.Sample/ViewController.designer.cs
@@ -0,0 +1,28 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace Ricardo.SDWebImage.iOS.Sample
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView ImageView { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (ImageView != null) {
+                ImageView.Dispose ();
+                ImageView = null;
+            }
+        }
+    }
+}
